04fa111658d4cd45d771a7e76a7c15c7f369fdb6
riza <r@r.com>
2026-06-14 11:25:41 +0530

    added plugins

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [], // Added empty plugins array to allow modification by Cloudflare/Wrangler
   build: {
     outDir: "dist",
     emptyOutDir: true,
